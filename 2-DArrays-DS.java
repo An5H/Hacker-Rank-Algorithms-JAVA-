f783fe9529c2277df8fe20c312f38509809e6c25
An5H <46046376+An5H@users.noreply.github.com>
2020-06-14 17:19:01 +0530

    Create 2-DArrays-DS.java

---
+++ 2-DArrays-DS.java
@@ -0,0 +1,61 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the hourglassSum function below.
+    static int hourglassSum(int[][] a) {
+        int maxSum = Integer.MIN_VALUE;
+            for(int i = 0; i < 6; i++)
+            {
+                for(int j = 0; j < 6; j++)
+                {
+                    if (i > 1 && j > 1)
+                    {
+                        int sum =
+                            a[i][j]
+                            + a[i][j-1]
+                            + a[i][j-2]
+                            + a[i-1][j-1]
+                            + a[i-2][j]
+                            + a[i-2][j-1]
+                            + a[i-2][j-2];
+                        if (sum > maxSum) {maxSum = sum;}
+                    }
+                }
+            }
+        return maxSum;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int[][] arr = new int[6][6];
+
+        for (int i = 0; i < 6; i++) {
+            String[] arrRowItems = scanner.nextLine().split(" ");
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            for (int j = 0; j < 6; j++) {
+                int arrItem = Integer.parseInt(arrRowItems[j]);
+                arr[i][j] = arrItem;
+            }
+        }
+
+        int result = hourglassSum(arr);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
