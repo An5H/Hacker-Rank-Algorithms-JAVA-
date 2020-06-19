b62ac7930a3f1fedd27091e950d36d6ae2b2b08a
An5H <46046376+An5H@users.noreply.github.com>
2020-06-19 13:20:03 +0530

    Create BigDecimal.java

---
+++ BigDecimal.java
@@ -0,0 +1,31 @@
+import java.math.BigDecimal;
+import java.util.*;
+class Solution{
+    public static void main(String []args){
+        //Input
+        Scanner sc= new Scanner(System.in);
+        int n=sc.nextInt();
+        String []s=new String[n+2];
+        for(int i=0;i<n;i++){
+            s[i]=sc.next();
+        }
+        sc.close();
+
+        Arrays.sort(s, Collections.reverseOrder(new Comparator<String>() {
+        @Override
+        public int compare(String a1, String a2) {
+            if(a1 == null || a2 == null){
+                return 0;
+            }
+            BigDecimal a = new BigDecimal(a1);
+            BigDecimal b = new BigDecimal(a2);
+            return a.compareTo(b);
+        }
+}));
+        //Output
+        for(int i=0;i<n;i++)
+        {
+            System.out.println(s[i]);
+        }
+    }
+}
